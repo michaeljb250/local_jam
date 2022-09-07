1fb299217bffbbe181dc09b028d3c63d0188d3c3
Michael Barnard <michaeljbarnard100@gmail.com>
2022-09-07 16:25:08 +0100

    djndfa

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# nigel = User.create!(
-#   email: "nigel@gmail.com",
-#   password: "bambam1",
-#   first_name: "Tom",
-#   last_name: "Jones",
-#   artist_name: "lil_jonesy",
-#   address: "10 downing street",
-#   instrument: "xylophone"
-#   )
+nigel = User.create!(
+  email: "nigel@gmail.com",
+  password: "bambam1",
+  first_name: "Tom",
+  last_name: "Jones",
+  artist_name: "lil_jonesy",
+  address: "10 downing street",
+  instrument: "xylophone",
+  bio: "hey hey, music is cool"
+  )
 
-  # jimothy = User.create!(
-  #   email: "jimothy@gmail.com",
-  #   password: "bambam1",
-  #   first_name: "pietro",
-  #   last_name: "sandwich",
-  #   artist_name: "lil_cup",
-  #   address: "1 london road",
-  #   instrument: "maracas"
-  #   )
+  jimothy = User.create!(
+    email: "jimothy@gmail.com",
+    password: "bambam1",
+    first_name: "pietro",
+    last_name: "sandwich",
+    artist_name: "lil_cup",
+    address: "1 london road",
+    instrument: "maracas",
+    bio: "Hi guys, I'm looking for a buddy to jam with sometime! Preferably a pianist, so get in touch!"
+  )
+
+
 
     beed = User.create!(
       email: "beedbarnardo@gmail.com",
@@ -33,5 +37,6 @@
       last_name: "barnard",
       artist_name: "lil_beedy",
       address: "11 langale drive",
-      instrument: "xylophone"
+      instrument: "xylophone",
+      bio: "Just want to play some funky tunes "
     )
