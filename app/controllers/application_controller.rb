ed5c749d409920e2b3586f32308b0c0c3cffed6b
Michael Barnard <michaeljbarnard100@gmail.com>
2022-09-08 11:18:02 +0100

    danialves

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :instrument, :photo])
   end
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
